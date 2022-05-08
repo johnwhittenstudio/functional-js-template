f48115bf3462228f5b3a19926d96c1c710576d53
John Whitten <johnwhitten.studio@gmail.com>
2022-05-08 15:11:50 -0700

    replace webpack to get successful build

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const Dotenv = require("dotenv-webpack");
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist"
   },
-  plugins: [
+  plugins: [ 
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Document',
+      title: 'jsDevEnvironmentTemplate',
       template: './src/index.html',
       inject: 'body'
     })
   ],
-  module: {
+  module: { 
     rules: [
-      {
-        test: /\.(svg|ico|gif|png|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/'
-            }
-          }
-        ]
-      },
-      {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
-      },
       {
         test: /\.css$/,
         use: [
@@ -63,7 +34,25 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
-      }
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
     ]
   }
 };
